ed8429fe4d94ab38a2d496fd1ffe02d218a57325
ShanMaqboolFab <shan.maqbool@fabtechsol.com>
2026-03-30 21:10:25 +0500

    api integration

--- app/src/main/java/com/example/pklocker/util/Constants.kt
+++ app/src/main/java/com/example/pklocker/util/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     // ─── SERVER CONFIGURATION ──────────────────────────────────────────
     // Current Local IP address for development
     // ─── PRODUCTION SERVER (VERCEL) ──────────────────────────────────────────
-    // const val BASE_URL = "https://pk-locker-api.vercel.app/api/"
-     const val BASE_URL = "http://192.168.100.5:5000/api/" // Local Dev
+    const val BASE_URL = "https://pk-locker-api.vercel.app/api/"
+    // const val BASE_URL = "http://192.168.100.5:5000/api/" // Local Dev
 }
